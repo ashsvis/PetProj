59c1261af337a16e2b381bca475bfc44611c0441
Александр Шаповалов <ashsvis@gmail.com>
2026-07-15 16:13:46 +0300

    Комментарии для методов отрезка

--- Line.cs
+++ Line.cs
@@ -1,119 +1,152 @@
 ﻿using System;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Xml.Linq;
 
 namespace PetProj
 {
     public class Line : Primitive
     {
+        /// <summary>
+        /// Конструктор для создания из редактора
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
         public Line(PointF point1, PointF point2) 
         { 
             Origin = point1;
             Offsets.Add(new SizeF(point2.X - point1.X, point2.Y - point1.Y));
         }
 
+        /// <summary>
+        /// Конструктор для создания при загрузке из файла
+        /// </summary>
+        /// <param name="xelement"></param>
         public Line(XElement xelement)
         {
             SetData(xelement);
         }
 
+        /// <summary>
+        /// Нарисовать отрезок заданным цветом
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="forecolor"></param>
         public override void DrawAt(Graphics graphics, Color forecolor)
         {
-            if (Offsets.Count == 1)
+            var pt1 = Origin;
+            var pt2 = Offsets.Count == 1 ? PointF.Add(pt1, Offsets[0]) : Origin;
+            using (var pen = new Pen(forecolor))
             {
-                var pt1 = Origin;
-                var pt2 = PointF.Add(pt1, Offsets[0]);
-                using (var pen = new Pen(forecolor))
-                {
-                    pen.StartCap = LineCap.Round;
-                    pen.EndCap = LineCap.Round;
-                    graphics.DrawLine(pen, pt1, pt2);
-                }
+                pen.StartCap = LineCap.Round;
+                pen.EndCap = LineCap.Round;
+                graphics.DrawLine(pen, pt1, pt2);
             }
         }
 
+        /// <summary>
+        /// Рисование выделенной линии с маркерами
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="forecolor"></param>
         public override void DrawSelectedAt(Graphics graphics, Color forecolor)
         {
-            if (Offsets.Count == 1)
+            var pt1 = Origin;
+            var pt2 = Offsets.Count == 1 ? PointF.Add(pt1, Offsets[0]) : Origin;
+            var selColor = Color.FromArgb(0, 127, 255);
+            using (var pen = new Pen(Color.FromArgb(80, selColor), 3))
+                graphics.DrawLine(pen, pt1, pt2);
+            using (var pen = new Pen(forecolor))
             {
-                var pt1 = Origin;
-                var pt2 = PointF.Add(pt1, Offsets[0]);
-                var middle = new PointF(pt1.X + (pt2.X - pt1.X) / 2f, pt1.Y + (pt2.Y - pt1.Y) / 2f);
-                var selColor = Color.FromArgb(0, 127, 255);
-                using (var pen = new Pen(Color.FromArgb(80, selColor), 3))
-                {
-                    graphics.DrawLine(pen, pt1, pt2);
-                }
-                using (var pen = new Pen(forecolor))
-                {
-                    pen.StartCap = LineCap.Round;
-                    pen.EndCap = LineCap.Round;
-                    graphics.DrawLine(pen, pt1, pt2);
-                }
-                using (var brush = new SolidBrush(selColor))
-                {
-                    graphics.FillRectangles(brush, new RectangleF[] { GetSquare(pt1), GetSquare(middle), GetSquare(pt2) });
-                }
-                using (var pen = new Pen(Color.Gray))
-                {
-                    graphics.DrawRectangles(pen, new RectangleF[] { GetSquare(pt1), GetSquare(middle), GetSquare(pt2) });
-                }
+                pen.StartCap = LineCap.Round;
+                pen.EndCap = LineCap.Round;
+                graphics.DrawLine(pen, pt1, pt2);
             }
+            using (var brush = new SolidBrush(selColor))
+                graphics.FillRectangles(brush, GetMarkers());
+            using (var pen = new Pen(Color.Gray))
+                graphics.DrawRectangles(pen, GetMarkers());
         }
 
+        /// <summary>
+        /// Получение массива маркерных квадратиков
+        /// </summary>
+        /// <returns></returns>
+        public RectangleF[] GetMarkers()
+        {
+            var pt1 = Origin;
+            var pt2 = Offsets.Count == 1 ? PointF.Add(pt1, Offsets[0]) : Origin;
+            var middle = new PointF(pt1.X + (pt2.X - pt1.X) / 2f, pt1.Y + (pt2.Y - pt1.Y) / 2f);
+            return new RectangleF[] { GetSquare(pt1), GetSquare(middle), GetSquare(pt2) };
+        }
+
+        /// <summary>
+        /// Вычисление размера маркерного квадратика
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
         private RectangleF GetSquare(PointF point)
         {
             var sz = new SizeF(6f, 6f);
             return new RectangleF(new PointF(point.X - sz.Width / 2f, point.Y - sz.Height / 2f), sz);
         }
 
+        /// <summary>
+        /// Рисование подсвеченного отрезка
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="forecolor"></param>
         public override void DrawHighlightAt(Graphics graphics, Color forecolor)
         {
-            if (Offsets.Count == 1)
+            var pt1 = Origin;
+            var pt2 = Offsets.Count == 1 ? PointF.Add(pt1, Offsets[0]) : Origin;
+            using (var pen = new Pen(Color.FromArgb(80, forecolor), 3))
+                graphics.DrawLine(pen, pt1, pt2);
+            using (var pen = new Pen(forecolor))
             {
-                var pt1 = Origin;
-                var pt2 = PointF.Add(pt1, Offsets[0]);
-                using (var pen = new Pen(Color.FromArgb(80, forecolor), 3))
-                {
-                    graphics.DrawLine(pen, pt1, pt2);
-                }
-                using (var pen = new Pen(forecolor))
+                pen.StartCap = LineCap.Round;
+                pen.EndCap = LineCap.Round;
+                graphics.DrawLine(pen, pt1, pt2);
+            }
+        }
+
+        /// <summary>
+        /// Точка аргумента попадает на отрезок
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public override bool Contains(PointF point)
+        {
+            var pt1 = Origin;
+            var pt2 = Offsets.Count == 1 ? PointF.Add(pt1, Offsets[0]) : Origin;
+            using (var path = new GraphicsPath())
+            {
+                path.AddLine(pt1, pt2);
+                using (var pen = new Pen(Color.Black, 3))
                 {
                     pen.StartCap = LineCap.Round;
                     pen.EndCap = LineCap.Round;
-                    graphics.DrawLine(pen, pt1, pt2);
+                    return path.IsOutlineVisible(point, pen);
                 }
             }
         }
 
-        public override bool Contains(PointF point)
-        {
-            if (Offsets.Count == 1)
-            {
-                var pt1 = Origin;
-                var pt2 = PointF.Add(pt1, Offsets[0]);
-                using (var path = new GraphicsPath())
-                {
-                    path.AddLine(pt1, pt2);
-                    using (var pen = new Pen(Color.Black, 3))
-                    {
-                        pen.StartCap = LineCap.Round;
-                        pen.EndCap = LineCap.Round;
-                        return path.IsOutlineVisible(point, pen);
-                    }
-                }
-            }
-            return false;
-        }
-
+        /// <summary>
+        /// Начальная точка отрезка
+        /// </summary>
         public PointF StartPoint => Origin;
+
+        /// <summary>
+        /// Конечная точка отрезка
+        /// </summary>
         public PointF EndPoint => Offsets.Count == 1 ? PointF.Add(Origin, Offsets[0]) : Origin;
 
+        /// <summary>
+        /// Коррекция начальной точки отрезка
+        /// </summary>
+        /// <param name="point"></param>
         public void MoveStartPointTo(PointF point)
         {
             var endPoint = EndPoint;
@@ -125,6 +158,10 @@ namespace PetProj
                 Offsets.Add(sz);
         }
 
+        /// <summary>
+        /// Коррекция конечной точки отрезка
+        /// </summary>
+        /// <param name="point"></param>
         public void MoveEndPointTo(PointF point)
         {
             var sz = new SizeF(point.X - Origin.X, point.Y - Origin.Y);
@@ -134,6 +171,10 @@ namespace PetProj
                 Offsets.Add(sz);
         }
 
+        /// <summary>
+        /// Получение данных конфигурации отрезка
+        /// </summary>
+        /// <returns></returns>
         public override XElement GetData()
         {
             var xline = new XElement("Line");
@@ -142,6 +183,10 @@ namespace PetProj
             return xline;
         }
 
+        /// <summary>
+        /// Восстановление данных конфигурации отрезка
+        /// </summary>
+        /// <param name="xelement"></param>
         private void SetData(XElement xelement)
         {
             CultureInfo culture = CultureInfo.CurrentCulture;
@@ -153,6 +198,12 @@ namespace PetProj
             Offsets.Add(new SizeF(point2.X - point1.X, point2.Y - point1.Y));
         }
 
+        /// <summary>
+        /// Разбор символьной записи для получения координат точки
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="decimalseparator"></param>
+        /// <returns></returns>
         private PointF Parse(string line, string decimalseparator)
         {
             // Разбиваем по запятым и убираем пустые элементы
